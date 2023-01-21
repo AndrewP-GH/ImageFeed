7433738073bed7f88981b94bf1c27e68a358f534
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-01-21 16:47:58 +0600

    Add colors

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extensions.swift
+//  ImageFeed
+//
+//  Created by Андрей Парамонов on 21.01.2023.
+//
+
+import Foundation
+
+extension UIColor {
+       static var ypBlack: UIColor { UIColor(named: "YP Black") }
+}
