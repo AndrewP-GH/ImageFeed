4464b121cb23971f38adfdfbb635b24b598566ed
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-01-22 04:45:37 +0600

    Add table view

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 class ImagesListViewController: UIViewController {
-
+    @IBOutlet private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
 }
 
