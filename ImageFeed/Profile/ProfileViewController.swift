8f2c09de12261214dfbb07f0f686b5edca7e37a3
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-02-24 20:57:46 +0600

    Add profile screen layout

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -9,4 +9,9 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    @IBOutlet var PersonImage: UIImageView!
+    @IBOutlet var FullNameLabel: UILabel!
+    @IBOutlet var NicknameLabel: UILabel!
+    @IBOutlet var TextLabel: UILabel!
+    @IBOutlet var LogoutButton: UIButton!
 }
