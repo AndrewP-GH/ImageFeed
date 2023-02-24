cf6fe8b0408bc3bc53c88ddcbf5fd2c404a298e3
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-02-24 19:01:44 +0600

    Add profile view controller

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Андрей Парамонов on 24.02.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+}
