0c9a967c637a962dfbe8be351eb901cd08c2ee86
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-02-26 21:42:21 +0600

    Add image view to navigation

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,9 @@
+//
+// Created by Андрей Парамонов on 26.02.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet private var imageView: UIImageView!
+}
