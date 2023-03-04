ed2b161f31658e888ee988305147f6e0cbabe1a2
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-03-05 02:06:55 +0600

    Add image zoom

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -13,16 +13,44 @@ final class SingleImageViewController: UIViewController {
             guard isViewLoaded else {
                 return
             }
-            imageView.image = image
+            setImage(image)
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.image = image
+
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+
+        setImage(image) // if image was set before view loaded
     }
 
     @IBAction private func didTapBackButton() {
         dismiss(animated: true)
+    }
+
+    private func setImage(_ image: UIImage) {
+        imageView.image = image
+        rescaleAndCenterImageInScrollView(image: image)
+    }
+
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let (minZoomScale, maxZoomScale) = (scrollView.minimumZoomScale, scrollView.maximumZoomScale)
+        view.layoutIfNeeded()
+        let visibleSize = scrollView.bounds.size
+        let (horizontalScale, verticalScale) = (visibleSize.width / image.size.width, visibleSize.height / image.size.height)
+        let scale = min(maxZoomScale, max(minZoomScale, max(horizontalScale, verticalScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let (contentWidth, contentHeight) = (scrollView.contentSize.width, scrollView.contentSize.height)
+        let (offsetX, offsetY) = ((contentWidth - visibleSize.width) / 2, (contentHeight - visibleSize.height) / 2)
+        scrollView.setContentOffset(CGPoint(x: offsetX, y: offsetY), animated: false)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
     }
 }
