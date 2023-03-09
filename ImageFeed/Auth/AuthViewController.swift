33cfae1847cc2dbad383ce622df1731c4c47a30e
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-03-10 02:43:32 +0600

    Add auth view controller

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Андрей Парамонов on 10.03.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+
+    @IBOutlet private var logoImageView: UIImageView!
+    @IBOutlet private var loginButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
