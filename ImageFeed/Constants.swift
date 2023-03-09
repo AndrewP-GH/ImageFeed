507fbe11e1757e0362088756f120b4a80a0e51aa
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-03-10 00:02:15 +0600

    Add constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,13 @@
+//
+// Created by Андрей Парамонов on 09.03.2023.
+//
+
+import Foundation
+
+struct Constants {
+    static let accessKey = "5HtTMQW-bcjYoItkTsXgT8FO5fUOdX9GiIFh_vbx7UI"
+    static let secretKey = "6SUreVK1nFltdeUC7JSoIBKjrfm30AECfbOCB5nN-L4"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
