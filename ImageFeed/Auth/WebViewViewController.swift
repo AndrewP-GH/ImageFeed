ad81f543b6daa969611424a45c1f86eeba5f3182
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-03-12 01:07:31 +0600

    Load auth page

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -3,11 +3,30 @@
 //
 
 import UIKit
-import WebKit 
+import WebKit
 
 final class WebViewViewController: UIViewController {
     @IBOutlet private var webView: WKWebView!
-    
+    private let unsplashAuthUrl = "https://unsplash.com/oauth/authorize"
+
     @IBAction private func didTapBackButton() {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadAuthPage()
+    }
+
+    private func loadAuthPage() {
+        var urlComponent = URLComponents(string: unsplashAuthUrl)!
+        urlComponent.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        let authUrl = urlComponent.url!
+        let urlRequest = URLRequest(url: authUrl)
+        webView.load(urlRequest)
     }
 }
