c95dd452b8164ac3540bc2d4118381eb38d26534
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-03-12 04:22:27 +0600

    Update progress bar

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -9,6 +9,7 @@ final class WebViewViewController: UIViewController {
     @IBOutlet private var webView: WKWebView!
     @IBOutlet private var progressView: UIProgressView!
     private let unsplashAuthUrl = "https://unsplash.com/oauth/authorize"
+    private let webViewProgressKeyPath = #keyPath(WKWebView.estimatedProgress)
 
     weak var delegate: WebViewViewControllerDelegate?
 
@@ -20,6 +21,31 @@ final class WebViewViewController: UIViewController {
         super.viewDidLoad()
         webView.navigationDelegate = self
         loadAuthPage()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        webView.addObserver(self, forKeyPath: webViewProgressKeyPath, options: .new, context: nil)
+        updateProgress()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        webView.removeObserver(self, forKeyPath: webViewProgressKeyPath)
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == webViewProgressKeyPath {
+            updateProgress()
+        }
+        else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+
+    private func updateProgress() {
+        progressView.progress = Float(webView.estimatedProgress)
+        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
     }
 
     private func loadAuthPage() {
