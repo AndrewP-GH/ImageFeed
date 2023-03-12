cab36531bd3fcc1e092a00d758224989ac661879
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-03-13 00:37:11 +0600

    Fix name

--- ImageFeed/Helpers/URLRequest+Extensions.swift
+++ ImageFeed/Helpers/URLRequest+Extensions.swift
@@ -4,13 +4,13 @@
 
 import Foundation
 
-fileprivate let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
+fileprivate let ApiBaseURL = URL(string: "https://api.unsplash.com")!
 
 extension URLRequest {
     static func makeHTTPRequest(
             path: String,
             httpMethod: String,
-            baseURL: URL = DefaultBaseURL
+            baseURL: URL = ApiBaseURL
     ) -> URLRequest {
         var request = URLRequest(url: URL(string: path, relativeTo: baseURL)!)
         request.httpMethod = httpMethod
