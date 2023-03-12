54ea98a90f64229fe4272290a5179e88dc26272b
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-03-12 15:17:47 +0600

    Implement token loading

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -30,10 +30,67 @@ final class AuthViewController: UIViewController {
 
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
-        //TODO: process code
+        let request = createAuthRequest(code: code)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            defer {
+                DispatchQueue.main.async { [weak self] in
+                    self?.dismiss(animated: true)
+                }
+            }
+            if let data,
+               let response = response as? HTTPURLResponse,
+               response.statusCode == 200 {
+                let authResponse = try? JSONDecoder().decode(AuthResponse.self, from: data)
+                if let authResponse = authResponse {
+                    UserDefaults.standard.set(authResponse.access_token, forKey: "access_token")
+                }
+            } else {
+                if let error {
+                    print("Auth error: \(error)")
+                }
+                if let response = response as? HTTPURLResponse {
+                    var msg = "no data"
+                    if let data {
+                        msg = String(data: data, encoding: .utf8) ?? "no message"
+                    }
+                    print("Auth error: \(response.statusCode), message: \(msg)")
+                }
+            }
+        }
+        task.resume()
     }
 
     func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
         dismiss(animated: true)
+    }
+
+    private func createAuthRequest(code: String) -> URLRequest {
+        let url = URL(string: "https://unsplash.com/oauth/token")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let authRequest = AuthRequest(
+                client_id: Constants.accessKey,
+                client_secret: Constants.secretKey,
+                redirect_uri: Constants.redirectURI,
+                code: code,
+                grant_type: "authorization_code")
+        request.httpBody = try? JSONEncoder().encode(authRequest)
+        return request
+    }
+
+    private struct AuthRequest: Encodable {
+        let client_id: String
+        let client_secret: String
+        let redirect_uri: String
+        let code: String
+        let grant_type: String
+    }
+
+    private struct AuthResponse: Decodable {
+        let access_token: String
+        let token_type: String
+        let scope: String
+        let created_at: Int
     }
 }
