c56144f34608d7a2cd34622d02c55077302b2a1d
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-03-12 16:29:10 +0600

    Finish service, still not work

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -12,6 +12,8 @@ final class AuthViewController: UIViewController {
 
     private let loginSegueIdentifier = "ShowWebView"
 
+    private let authService = OAuth2Service()
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -30,68 +32,22 @@ final class AuthViewController: UIViewController {
 
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
-        let request = createAuthRequest(code: code)
-        let session = URLSession.shared
-        let task = session.dataTask(with: request) { data, response, error in
+        authService.fetchAuthToken(code: code) { [weak self] result in
             defer {
                 DispatchQueue.main.async { [weak self] in
                     self?.dismiss(animated: true)
                 }
             }
-            if let data,
-               let response = response as? HTTPURLResponse,
-               response.statusCode == 200 {
-                let authResponse = try? JSONDecoder().decode(AuthResponse.self, from: data)
-                if let authResponse = authResponse {
-                    UserDefaults.standard.set(authResponse.access_token, forKey: "access_token")
-                }
-            } else {
-                if let error {
-                    print("Auth error: \(error)")
-                }
-                if let response = response as? HTTPURLResponse {
-                    var msg = "no data"
-                    if let data {
-                        msg = String(data: data, encoding: .utf8) ?? "nil"
-                    }
-                    print("Auth error: \(response.statusCode), message: \(msg)")
-                }
+            switch result {
+            case .success(let token):
+                print("Token: \(token)")
+            case .failure(let error):
+                print("Error: \(error)")
             }
         }
-        task.resume()
     }
 
     func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
         dismiss(animated: true)
-    }
-
-    private func createAuthRequest(code: String) -> URLRequest {
-        let url = URL(string: "https://unsplash.com/oauth/token")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        let authRequest = AuthRequest(
-                client_id: Constants.accessKey,
-                client_secret: Constants.secretKey,
-                redirect_uri: Constants.redirectURI,
-                code: code,
-                grant_type: "authorization_code")
-        let body = try? JSONEncoder().encode(authRequest)
-        request.httpBody = body
-        return request
-    }
-
-    private struct AuthRequest: Encodable {
-        let client_id: String
-        let client_secret: String
-        let redirect_uri: String
-        let code: String
-        let grant_type: String
-    }
-
-    private struct AuthResponse: Decodable {
-        let access_token: String
-        let token_type: String
-        let scope: String
-        let created_at: Int
     }
 }
---
+++ ImageFeed/Auth/OAuth2Service.swift
@@ -0,0 +1,64 @@
+//
+// Created by Андрей Парамонов on 12.03.2023.
+//
+
+import Foundation
+
+final class OAuth2Service {
+    func fetchAuthToken(code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        let request = createAuthRequest(code: code)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            if let data = data,
+               let response = response as? HTTPURLResponse,
+               200..<300 ~= response.statusCode {
+                let authResponse = try? JSONDecoder().decode(AuthResponse.self, from: data)
+                if let authResponse {
+                    completion(.success(authResponse.access_token))
+                }
+            } else {
+                if let response = response as? HTTPURLResponse {
+                    var msg = "no data"
+                    if let data = data {
+                        msg = String(data: data, encoding: .utf8) ?? "nil"
+                    }
+                    completion(.failure(NSError(domain: "Auth error: \(msg)", code: response.statusCode)))
+                } else {
+                    completion(.failure(error ?? NSError(domain: "Unknown error", code: 0)))
+                }
+            }
+        }
+        task.resume()
+    }
+
+    private func createAuthRequest(code: String) -> URLRequest {
+        let url = URL(string: "https://unsplash.com/oauth/token")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let authRequest = AuthRequest(
+                client_id: Constants.accessKey,
+                client_secret: Constants.secretKey,
+                redirect_uri: Constants.redirectURI,
+                code: code,
+                grant_type: "authorization_code")
+        let body = try? JSONEncoder().encode(authRequest)
+        print(String(data: body!, encoding: .utf8) ?? "nil")
+        request.httpBody = body
+        return request
+    }
+
+    private struct AuthRequest: Encodable {
+        let client_id: String
+        let client_secret: String
+        let redirect_uri: String
+        let code: String
+        let grant_type: String
+    }
+
+    private struct AuthResponse: Decodable {
+        let access_token: String
+        let token_type: String
+        let scope: String
+        let created_at: Int
+    }
+}
