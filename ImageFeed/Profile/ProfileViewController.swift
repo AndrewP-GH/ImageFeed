75f5783035d2b43f8b379513503c6eaad4316f3c
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-03-26 04:54:48 +0600

    Remove assert

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -71,7 +71,6 @@ final class ProfileViewController: UIViewController {
         addSubViews()
         applyConstraints()
         guard let profile = profileService.profile else {
-            assertionFailure("Profile is not loaded")
             return
         }
         updateProfileDetails(profile: profile)
