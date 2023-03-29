664e4533a8a0e926d4c1c57e729291589d163346
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-03-29 22:11:29 +0300

    Add test

---
+++ ImageFeedTests/ImagesListServiceTests.swift
@@ -0,0 +1,15 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Андрей Парамонов on 29.03.2023.
+//
+
+@testable import ImageFeed
+import XCTest
+
+final class ImagesListServiceTests: XCTestCase {
+    func testExample() {
+        
+    }
+}
