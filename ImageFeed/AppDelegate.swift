dd4d3e869c19f500c1a5af68edf1c19fc40993df
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-04-05 00:25:32 +0300

    Style progress hud

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import ProgressHUD
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        ProgressHUD.animationType = .systemActivityIndicator
+        ProgressHUD.colorHUD = .ypBlack
+        ProgressHUD.colorAnimation = .lightGray
         return true
     }
 
